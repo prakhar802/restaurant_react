f976ae8574c8df2a8afa218209d59a120dcadae6
prakhar jain <prakharmohanjain@gmail.com>
2023-01-02 12:50:10 +0530

    reactjs Project first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+// import Resturant from "./component/Basics/Resturant";cd 
+// import UseState from "./component/Hooks/useState";
+// import UseEffect from "./component/Hooks/useEffect";
+//import UseReducer from "./component/Hooks/useReducer";
+import Resturant from "./component/basics/Resturant";
+
+const App = () => {
+  return <Resturant />;
+};
+
+export default App;
